f96d88100067da0a14e00621419bc11996f74fdd
shirzlotnik <55185579+shirzlotnik@users.noreply.github.com>
2019-11-10 21:25:40 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle[] boardColumns;
+        private int[,] board;
+        public Form1()
+        {
+            InitializeComponent();
+            this.boardColumns = new Rectangle[7];
+        }
+
+        private void From1_Load(object sender,EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.DarkGray, 24, 24, 340, 300);
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i == 0)
+                        this.boardColumns[j] = new Rectangle(32 + 48 * j, 24, 32, 300);
+                    e.Graphics.FillEllipse(Brushes.White, 32 + 48 * j, 32 + 48 * i, 32, 32);
+                }
+            }
+        }
+    }
+}
